51b3bd01e2517a176618114d021544ed020b0b17
A-Reza <arezajafri@gmail.com>
2024-10-16 12:41:05 +0400

    add c3 tresitter parser

--- init.lua
+++ init.lua
@@ -992,5 +992,23 @@ require('lazy').setup({
   },
 })
 
+vim.filetype.add {
+  extension = {
+    c3 = 'c3',
+    c3i = 'c3',
+    c3t = 'c3',
+  },
+}
+
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.c3 = {
+  install_info = {
+    url = 'https://github.com/c3lang/tree-sitter-c3',
+    files = { 'src/parser.c', 'src/scanner.c' },
+    branch = 'main',
+  },
+  filetype = 'c3',
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
